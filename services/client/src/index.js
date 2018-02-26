b587ba139f8366630910597982bf7077c79aefab
tjneisi <tjneisi@gmail.com>
2018-02-25 19:02:21 -0800

    Create AddUser component and write tests for it

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
 
@@ -30,6 +31,8 @@ class App extends Component {
             <br/>
             <h1>All Users</h1>
             <hr/><br/>
+              <AddUser/>
+              <br/>
               <UsersList users={this.state.users}/>
           </div>
         </div>
